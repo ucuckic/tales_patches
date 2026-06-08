87382a9044f961198b0b9e40ad4f78de6be1a59e
ucuckic <37884144+ucuckic@users.noreply.github.com>
2026-06-08 10:44:20 -0500

    t

---
+++ ppsspp/ndx/_patch_cpp/0x08820FA4_cycle_control_characters_upd.cpp
@@ -0,0 +1,55 @@
+//compiler options used:
+//-mno-abicalls -march=allegrex -mabi=eabi -G0  -fomit-frame-pointer
+
+#include <stdint.h>
+
+extern "C" void  z_un_08817630(uintptr_t state, int arg);
+extern "C" void* pos_0x09F00D00(int index); // get_char_by_index
+
+void shift_party_left(int shifter)
+{
+    uintptr_t mainPtr = 0x08AF1F30;
+    int partySize = *(int8_t*)((uintptr_t)mainPtr + 0x5F4);
+
+    if (partySize > 1)
+    {
+        int8_t* chars    = (int8_t*)0x081E0030;
+        int8_t* ctrlMode = (int8_t*)0x08B069C4;
+
+        int forcedIndex = shifter;
+
+        int first = -1;
+
+        for (int i = 0; i < partySize; i++)
+        {
+            if (i == forcedIndex || ctrlMode[i] == 0)
+            {
+                first = i;
+                break;
+            }
+        }
+
+        if (first >= 0)
+        {
+            int8_t saved = chars[first];
+            int prev = first;
+
+            for (int i = first + 1; i < partySize; i++)
+            {
+                if (i == forcedIndex || ctrlMode[i] == 0)
+                {
+                    chars[prev] = chars[i];
+                    int8_t* charObj = (int8_t*)pos_0x09F00D00(prev);
+                    charObj[0x35] = ctrlMode[prev];
+                    prev = i;
+                }
+            }
+
+            chars[prev] = saved;
+            int8_t* charObj = (int8_t*)pos_0x09F00D00(prev);
+            charObj[0x35] = ctrlMode[prev];
+        }
+    }
+
+    z_un_08817630(mainPtr, 0);
+}
--- ppsspp/ndx/_patch_cpp/0x09F20900_extra_btl_menu_inputs.cpp
+++ ppsspp/ndx/_patch_cpp/0x09F20900_extra_btl_menu_inputs.cpp
@@ -11,30 +11,37 @@ extern "C" void* z_un_09F00D00(int index); // get_char_by_index
 void btl_menu_extra_input_checks()
 {
     uintptr_t mainPtr = 0x08AF1F30;
+
+    uint8_t gameState = *(uint8_t*)(mainPtr+0x5ED);
+
+    if( !(gameState == 0x0F || gameState == 0x17) ) return; //return if not btl menu or pause
+
     uintptr_t cMemPtr = 0x081E0000;
 
     uint32_t menuOwner = *(uint32_t*)(cMemPtr+0x4);
-    uint8_t partySize = *(uint16_t*)(mainPtr+0x5f4);
+    uint8_t partySize = *(uint8_t*)(mainPtr+0x5f4);
 
     uint16_t selButtonBind = *(uint16_t*)(mainPtr+0x2e);
     uint16_t lButtonBind = *(uint16_t*)(mainPtr+0x42);
 
     for(int i = 0; i < partySize; i++)
     {
-        if(i == menuOwner) continue; //skip the person who opened the menu because they already have these controls
-
         int bMask = get_btn_0x4850(0,i);
+
+        //char cycle
+        if( (bMask&lButtonBind)!=0 )
+        {
+            pos_08820FA4(i);
+        }
+
+        //past this point - everyone EXCEPT menu host
+        //if(i == menuOwner) continue; //skip the person who opened the menu because they already have these controls
+        //fuck it rip out the original
 
         //mode change
         if( (bMask&selButtonBind)!=0 )
         {
             pos_0x09F10800(i);
-        }
-
-        //char cycle
-        if( (bMask&lButtonBind)!=0 )
-        {
-            pos_08820FA4(mainPtr);
         }
     }
 }
